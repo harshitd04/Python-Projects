cd9d1177a2968563934058229f519494b84b7d25
Harshit Dabas <harshitd2004@gmail.com>
2025-02-17 21:48:36 +0530

    With Error Handling

--- NATO_main.py
+++ NATO_main.py
@@ -1,17 +1,36 @@
 import pandas
+try:
+    nato_file = pandas.read_csv(r"C:\Users\RAKESH KUMAR DABAS\PycharmProjects\PythonProject\day-26\.venv\NATO-alphabet-start"
+                            r"\NATO-alphabet-start\NATO_nato_phonetic_alphabet.csv")
 
-nato_file = pandas.read_csv(r"C:\Users\RAKESH KUMAR DABAS\PycharmProjects\PythonProject\day-26\.venv"
-                            r"\NATO-alphabet-start\NATO-alphabet-start\nato_phonetic_alphabet.csv")
+    nato_dict = {row.letter:row.code for (index,row) in nato_file.iterrows()}
+    # print(nato_dict.items())
+    def generate_phonetic():
+        try:
+            user_input = input("Enter your word: ").upper()
+            output_list = [nato_dict[letter] for letter in user_input]
+        except KeyError:
+            print(f"Only letters in the alphabet.")
+            generate_phonetic()
+        else:
+            print(output_list)
+
+except FileNotFoundError:
+    print("File Not Found. Please Check")
+
+else:
+    generate_phonetic()
+
+
+
+
+
+
+
+
+
 
 # nato_dict = {row.letter:row.code for (letter,code) in nato_file.iterrows()}     #Wrong way
-
-nato_dict = {row.letter:row.code for (index,row) in nato_file.iterrows()}
-# print(nato_dict.items())
-
-
-user_input = input("Enter your word: ").upper()
-output_list = [nato_dict[letter] for letter in user_input]
-print(output_list)
 
 # output_list = [value for key,value in nato_dict.items() if key in user_input]   #Wrong way
 # user_input.t0_list()
